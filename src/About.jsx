eeb73c1f8e33d2fee4ce41bdcdab2bdae746f3f8
nirmal100102 <nirmalpatil.1001@gmail.com>
2022-02-11 15:49:37 +0530

    build changes

--- src/About.jsx
+++ src/About.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import web from "../src/images/img3.jpeg"
 import Common from "./Common";
 
--- src/Card.jsx
+++ src/Card.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import web from "../src/images/img2.jpg"
+
 
 const Card = (props) => {
     return(
